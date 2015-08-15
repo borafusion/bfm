5a7f60a8ec822127aad7a0d1db386291322cc625
riruvuri <irchowdary@gmail.com>
2015-08-14 20:51:49 -0700

    Inward LC Integration

--- BankFacilityMgmt/BfmVC/src/rbmbfm/view/LoginBean.java
+++ BankFacilityMgmt/BfmVC/src/rbmbfm/view/LoginBean.java
@@ -103,6 +103,19 @@ public class LoginBean {
         }
         return null;
     }
+    
+    public String onLogout() {
+        FacesContext fctx = FacesContext.getCurrentInstance();
+        ExternalContext ectx = fctx.getExternalContext();
+        String url = ectx.getRequestContextPath() + "/adfAuthentication?logout=true&end_url=/faces/Login.jspx";
+        try {
+            ectx.redirect(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        fctx.responseComplete();
+        return null;
+    }    
 
     private void reportUnexpectedLoginError(String errType, Exception e) {
         FacesMessage msg =
